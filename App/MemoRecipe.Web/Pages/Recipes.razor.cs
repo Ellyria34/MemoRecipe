d1d7d8eb247f71b566e008ff87c84528a22f5e57
Sarah LEON <leon.sarah24@gmail.com>
2026-03-30 19:13:53 +0200

    refactor(frontend): extract Recipes list code-behind into separate .razor.cs file

---
+++ App/MemoRecipe.Web/Pages/Recipes.razor.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using MemoRecipe.Web.Models;
+using MemoRecipe.Web.Services;
+
+namespace MemoRecipe.Web.Pages;
+
+public partial class Recipes
+{
+    [Inject]
+    private IRecipeService RecipeService {get; set;} = null!;
+
+    [Inject]
+    private NavigationManager Navigation {get; set;} = null!;
+
+    List<RecipeDto>? _recipes = null;
+    bool _isLoading = false;
+    string? _errorMessage = null;
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        _isLoading = true;
+        _errorMessage = null;
+        try
+        {
+           _recipes = await RecipeService.GetAllRecipesAsync();
+        }
+        catch (Exception)
+        {
+            _errorMessage = "Un problème est survenu lors du chargement de vos recettes";
+        }
+        finally
+        {
+            _isLoading = false;
+        }
+    }
+
+    private async Task HandleRedirection()
+    {
+        Navigation.NavigateTo($"/recipes/new");
+    }
+}
